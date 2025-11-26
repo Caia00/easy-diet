1da78132737c7dbc3e837f60b6743f1b8ea84a63
Caia00 <dancaianiell@gmail.com>
2025-11-26 11:18:05 +0100

    defining module: nutritional values

---
+++ main-app/src/main/java/models/nutritionalValues.java
@@ -0,0 +1,62 @@
+package models;
+
+public class nutritionalValues {
+    private double kcal;
+    private double carbs;
+    private double sugar;
+    private double fats;
+    private double fibers;
+    private double proteins;
+
+    public nutritionalValues() {}
+    public nutritionalValues(double kcal, double carbs, double sugar, double fats, double fibers, double proteins) {
+        this.kcal = kcal;
+        this.carbs = carbs;
+        this.sugar = sugar;
+        this.fats = fats;
+        this.fibers = fibers;
+        this.proteins = proteins;
+    }
+
+    public nutritionalValues values(){
+        return this;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+    public void setKcal(double kcal) {
+        this.kcal = kcal;
+    }
+    public double getCarbs() {
+        return carbs;
+    }
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+    public double getSugar() {
+        return sugar;
+    }
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+    public double getFats() {
+        return fats;
+    }
+    public void setFats(double fats) {
+        this.fats = fats;
+    }
+    public double getFibers() {
+        return fibers;
+    }
+    public void setFibers(double fibers) {
+        this.fibers = fibers;
+    }
+    public double getProteins() {
+        return proteins;
+    }
+    public void setProteins(double proteins) {
+        this.proteins = proteins;
+    }
+
+}
