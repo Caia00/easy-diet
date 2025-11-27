797536da98e93c39bc40fa2ed82c1b854845bb41
Caia00 <dancaianiell@gmail.com>
2025-11-27 10:43:23 +0100

    little modify: changed NutritionalTarget structure using composition with nutritionalValues

--- main-app/src/main/java/models/NutritionalTarget.java
+++ main-app/src/main/java/models/NutritionalTarget.java
@@ -3,23 +3,13 @@ package models;
 public class NutritionalTarget {
 
         private appCategory category;
+        private nutritionalValues nutritionalValues;
 
-        private double targetKcal;
-        private double targetProteins;
-        private double targetCarbs;
-        private double targetFats;
-        private double targetFibers;
-        private double targetSugar;
 
         public NutritionalTarget(appCategory category, double targetKcal, double targetProteins,
                                  double targetCarbs, double targetFats, double targetFibers, double targetSugar) {
+            this.nutritionalValues = new nutritionalValues(targetKcal, targetCarbs, targetSugar, targetFats, targetFibers, targetProteins);
             this.category = category;
-            this.targetKcal = targetKcal;
-            this.targetProteins = targetProteins;
-            this.targetCarbs = targetCarbs;
-            this.targetFats = targetFats;
-            this.targetFibers = targetFibers;
-            this.targetSugar = targetSugar;
         }
 
         public appCategory getCategory() {
@@ -27,27 +17,27 @@ public class NutritionalTarget {
         }
 
         public double getTargetKcal() {
-            return targetKcal;
+            return nutritionalValues.getKcal();
         }
 
         public double getTargetProteins() {
-            return targetProteins;
+            return nutritionalValues.getProteins();
         }
 
         public double getTargetCarbs() {
-            return targetCarbs;
+            return nutritionalValues.getCarbs();
         }
 
         public double getTargetFats() {
-            return targetFats;
+            return nutritionalValues.getFats();
         }
 
         public double getTargetFibers() {
-            return targetFibers;
+            return nutritionalValues.getFibers();
         }
 
         public double getTargetSugar() {
-            return targetSugar;
+            return nutritionalValues.getSugar();
         }
 
 }
