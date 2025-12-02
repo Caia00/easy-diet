fb875efc0b575b19bf029e22a6a76aa51953ff70
Caia00 <dancaianiell@gmail.com>
2025-12-02 20:17:36 +0100

    defining view: only defined GuiAuthView

---
+++ main-app/src/main/java/view/GuiAuthView.java
@@ -0,0 +1,35 @@
+package view;
+
+import controller.LoginController;
+
+import javax.swing.text.View;
+
+public class GuiAuthView implements AuthView {
+
+    @Override
+    public void setController(LoginController controller) {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void showErrorMessage(String message){
+
+    }
+
+    @Override
+    public void showSuccessMessage(String message){
+
+    }
+
+    @Override
+    public void close(){
+
+    }
+
+
+}
