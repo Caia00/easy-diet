c9a9cf5779c1c7d3106408d4948b0507f5d1edda
Caia00 <dancaianiell@gmail.com>
2025-12-30 18:02:57 +0100

    implemented test: created test cases for CsvCatalogLoader using a dummy catalog.csv

---
+++ main-app/src/test/java/models/services/CsvCatalogLoaderTest.java
@@ -0,0 +1,70 @@
+package models.services;
+
+import models.AppCategory;
+import models.CommercialProduct;
+import models.factory.ProductFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CsvCatalogLoaderTest {
+    private List<CommercialProduct> loadedProducts;
+
+    //Metodo setup per i test, caricamento catalogo e salvataggio lista in attributo di classe
+    @BeforeEach
+    void setUp() {
+        ProductFactory realFactory = new ProductFactory();
+        CsvCatalogLoader loader = new CsvCatalogLoader("test_catalog.csv", realFactory);
+        this.loadedProducts = loader.loadCatalog();
+    }
+
+
+    @Test
+    @DisplayName("Caricamento: Verifica il numero corretto di prodotti importati")
+    void testLoadCatalogAndParseLineShouldLoadCorrectNumberOfProducts() {
+
+        assertNotNull(loadedProducts, "La lista non deve essere null");
+
+        //Il file test_catalog.csv ha 4 righe di dati, di cui una non valida
+        assertEquals(3, loadedProducts.size(), "Dovrebbe aver caricato esattamente 3 prodotti validi");
+    }
+
+
+    @Test
+    @DisplayName("Parsing: Verifica che i dettagli dei prodotti siano convertiti correttamente")
+    void testParseDoubleSafeAndParseKcalShouldParseProductDetailsCorrectly() {
+
+        //Verifica rigatoni, parsing standard
+        CommercialProduct p1 = loadedProducts.get(0);
+        assertEquals("Rigatoni Barilla", p1.getName());
+        assertEquals(AppCategory.PASTA, p1.getCategory());
+        assertEquals(350.0, p1.getKcalPer100g(), 0.1);
+
+        //Verifica mela, presenza virgola e scritta kcal
+        CommercialProduct p2 = loadedProducts.get(1);
+        assertEquals("Mela Fuji", p2.getName());
+        assertEquals(0.80, p2.getPrice(), 0.01, "Prezzo con virgola errato");
+        assertEquals(52.0, p2.getKcalPer100g(), 0.1, "Parsing '52 kcal' errato");
+
+        //Verifica pollo, presenza struttura complessa Kj / kcal
+        CommercialProduct p3 = loadedProducts.get(2);
+        assertEquals("Pollo Arrosto", p3.getName());
+        assertEquals(100.0, p3.getKcalPer100g(), 0.1, "Parsing regex '150 kj / 100 kcal' errato");
+    }
+
+
+    @Test
+    @DisplayName("FILE MISSING: Restituisce lista vuota se il file manca")
+    void shouldReturnEmptyListIfFileMissing() {
+        //Qui non si usa il setup in quanto vogliamo un file sbagliato apposta
+        ProductFactory factory = new ProductFactory();
+        CsvCatalogLoader badLoader = new CsvCatalogLoader("file_inesistente.csv", factory);
+
+        List<CommercialProduct> emptyList = badLoader.loadCatalog();
+
+        assertTrue(emptyList.isEmpty(), "Deve restituire lista vuota se file manca");
+    }
+}
