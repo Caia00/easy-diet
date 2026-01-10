48cab7a1e2355069748c95af36accdc43aa20ee2
Caia00 <dancaianiell@gmail.com>
2026-01-10 10:41:19 +0100

    little changes: resolving issues

--- main-app/src/main/java/models/services/DatabaseConnection.java
+++ main-app/src/main/java/models/services/DatabaseConnection.java
@@ -1,20 +1,23 @@
 package models.services;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.*;
 
 public class DatabaseConnection {
     private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
     private static DatabaseConnection instance;
     private Connection connection;
-    private final String url = "jdbc:mysql://localhost:3306/easy_diet_db";
-    private final String username = "caia";
-    private final String password = "";
+    private String url;
+    private String username;
+    private String password;
 
     private DatabaseConnection() throws SQLException {
         try {
+            loadConfig();
             Class.forName("com.mysql.cj.jdbc.Driver");
             this.connection = DriverManager.getConnection(url, username, password);
             logger.info("Connessione al DB MySQL riuscita!");
@@ -28,6 +31,23 @@ public class DatabaseConnection {
             instance = new DatabaseConnection();
         }
         return instance;
+    }
+
+    public void loadConfig() {
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Spiacente, non trovo config.properties");
+                return;
+            }
+            prop.load(input);
+            this.password = prop.getProperty("db.password");
+            this.username = prop.getProperty("db.user");
+            this.url = prop.getProperty("db.url");
+
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Errore nella configurazione delle properties", ex);
+        }
     }
 
     public Connection getConnection() {
